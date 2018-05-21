518ab6526ef59cddeadd9722900529559bec449c
bucong <531053266@qq.com>
2018-05-21 10:07:54 +0800

    sdk引入

--- src/views/login/register.vue
+++ src/views/login/register.vue
@@ -10,7 +10,10 @@
         <p>{{ $t('register.info') }}</p>
       </div>
       <div class="sub-box">
-        <button v-clipboard:copy="password" v-clipboard:success="onCopy" v-clipboard:error="onError">{{ $t("register.step1.btn1") }}</button>
+        <button @click="downloadTXT">下载</button>
+        <button v-clipboard:copy="password" v-clipboard:success="onCopy" v-clipboard:error="onError">{{
+          $t("register.step1.btn1") }}
+        </button>
         <button @click="next">{{ $t("register.step1.btn2") }}</button>
       </div>
     </div>
@@ -32,48 +35,61 @@
 
 <script>
   import {handleLocalStorage} from '../../utils/util'
-export default {
-  name: 'index',
-  data () {
-    return {
-      password: 'property dune human cluster penalty police monitor mammal blush license civil vault', //生成的密码
-      subPassword: '', //填写的密码
-      step: 1, //步骤1，步骤2
-      err: false,  //登录出错
-    }
-  },
-  beforeCreate () {
-    document.querySelector('body').setAttribute('style', 'background-image: url(/static/img/login-bg.png);min-width: 700px;');
-  },
-  methods: {
-    next(){
-      this.step = 2;
-      this.err = false;
-      this.subPassword = '';
-    },
-    back(){
-      this.step = 1;
-    },
-    loginSub(){
-      //判断密码
-      if(this.password.length > 20){
-        handleLocalStorage('set','login','yes');
-        this.$router.push({path: '/'})
-      }else{
-        this.err = true;
+  import {crypto} from 'htc-js'
+  import {saveAs} from '../../assets/js/FileSaver.min'
+  import {fetchTest} from '@/api/api'
+  export default {
+    name: 'index',
+    data() {
+      return {
+        password: '', //生成的密码
+        subPassword: '', //填写的密码
+        step: 1, //步骤1，步骤2
+        err: false,  //登录出错
       }
     },
-    onCopy(){ //复制成功
-      this.$message({
-        message: this.$t('register.copy.success'),
-        type: 'success'
-      });
+    beforeCreate() {
+      document.querySelector('body').setAttribute('style', 'background-image: url(/static/img/login-bg.png);min-width: 700px;');
     },
-    onError(){ //复制失败
-      this.$message.error(this.$t('register.copy.fail'));
+    created() {
+      this.password = crypto.generateSecret();
+      fetchTest().then(res => {
+        console.log(res.data)
+      })
+    },
+    methods: {
+      next() {
+        this.step = 2;
+        this.err = false;
+        this.subPassword = '';
+      },
+      back() {
+        this.step = 1;
+      },
+      loginSub() {
+        //判断密码
+        if (this.password.length > 20) {
+          handleLocalStorage('set', 'login', 'yes');
+          this.$router.push({path: '/'})
+        } else {
+          this.err = true;
+        }
+      },
+      onCopy() { //复制成功
+        this.$message({
+          message: this.$t('register.copy.success'),
+          type: 'success'
+        });
+      },
+      onError() { //复制失败
+        this.$message.error(this.$t('register.copy.fail'));
+      },
+      downloadTXT() { //下载密码TXT文件
+        let blob = new Blob([this.password], {type: "text/plain;charset=utf-8"});
+        saveAs(blob, "password.txt");
+      }
     }
   }
-}
 </script>
 
 <style scoped>
